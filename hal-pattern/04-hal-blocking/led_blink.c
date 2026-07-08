b168b80387c9d4b3ffeff43b637584c14df2de75
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-07-08 21:37:29 +0700

    [ADD] hal-pattern/04-hal-blocking from examples/04-hal-c

---
+++ hal-pattern/04-hal-blocking/led_blink.c
@@ -0,0 +1,69 @@
+#include "led_blink.h"
+
+int main(void);
+extern void _estack(void);
+
+__attribute__((naked, noreturn)) void Reset_Handler(void)
+{
+	extern long _sbss, _ebss, _sdata, _edata, _sidata;
+
+	for (long* dst = &_sbss; dst < &_ebss; dst++)
+	{
+		*dst = 0;
+	}
+
+	for (long *dst = &_sdata, *src = &_sidata; dst < &_edata;)
+	{
+		*dst++ = *src++;
+	}
+
+	main();
+	for (;;)
+	{
+	}
+}
+
+void SysTick_Handler(void)
+{
+	HAL_IncTick();
+}
+
+__attribute__((section(".isr_vector"))) void (*const g_pfnVectors[16])(void) = {
+    _estack,
+    Reset_Handler,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    SysTick_Handler,
+};
+
+int main(void)
+{
+	HAL_Init();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+
+	GPIO_InitTypeDef gpio_init = {
+	    .Pin   = LED_PIN,
+	    .Mode  = GPIO_MODE_OUTPUT_PP,
+	    .Pull  = GPIO_NOPULL,
+	    .Speed = GPIO_SPEED_FREQ_LOW,
+	};
+	HAL_GPIO_Init(LED_PORT, &gpio_init);
+
+	for (;;)
+	{
+		HAL_GPIO_TogglePin(LED_PORT, LED_PIN);
+		HAL_Delay(TICK_MS);
+	}
+	return 0;
+}
---
+++ hal-pattern/04-hal-blocking/led_blink.h
@@ -0,0 +1,14 @@
+#ifndef __LED_BLINK_H__
+#define __LED_BLINK_H__
+
+/*
+ * stm32l1xx_hal.h includes stm32l1xx.h (chip register maps from CMSIS-Device)
+ * and every HAL module enabled in stm32l1xx_hal_conf.h.
+ */
+#include "stm32l1xx_hal.h"
+
+#define LED_PORT GPIOB
+#define LED_PIN  GPIO_PIN_8
+#define TICK_MS  100U
+
+#endif // __LED_BLINK_H__
