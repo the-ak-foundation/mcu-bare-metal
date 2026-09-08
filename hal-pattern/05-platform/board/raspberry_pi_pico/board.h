32485f38532d2453621789f7338833f2f2b26c75
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-09-08 23:51:55 +0700

    [BOARD] add Raspberry Pi Pico board.h with LED pin

---
+++ hal-pattern/05-platform/board/raspberry_pi_pico/board.h
@@ -0,0 +1,11 @@
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
+#include "board_init.h"
+#include "board_leds.h"
+
+#define BOARD_RASPBERRY_PI_PICO
+
+#define RASPBERRY_PI_PICO_LED_PIN    ((bsp_io_port_pin_t) 25)   /* GPIO25 */
+
+#endif // __BOARD_H__
