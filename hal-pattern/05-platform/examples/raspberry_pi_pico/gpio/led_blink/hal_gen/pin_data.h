0be4b53c1b944fb7d63f4dfacea319a0cbf893ed
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-09-11 01:42:33 +0700

    [APP] add Pico led_blink pin_data.h

---
+++ hal-pattern/05-platform/examples/raspberry_pi_pico/gpio/led_blink/hal_gen/pin_data.h
@@ -0,0 +1,13 @@
+#ifndef __PIN_DATA_H__
+#define __PIN_DATA_H__
+
+#include "board.h"
+#include "hal_gpio_api.h"
+
+HAL_HEADER
+
+extern const hal_gpio_cfg_t g_bsp_pin_cfg;
+
+HAL_FOOTER
+
+#endif // __PIN_DATA_H__
